c7f457ac0160319ba146bfbf05a016755b54a3f9
羅子淇 Jacky Law, F.I.H.R.M.(HK), FHKIoD <289607996+jackylawck@users.noreply.github.com>
2026-08-17 11:15:21 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,28 @@
-const CACHE_NAME = 'mindful-vault-v1';
+const CACHE_NAME = 'stillhollow-v2';
 const ASSETS = [
     './',
-    './index.html'
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './Stillhollowicon-192.png',
+    './Stillhollowicon-512.png'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
     self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => Promise.all(
+            keys.map((k) => k !== CACHE_NAME && caches.delete(k))
+        ))
     );
     self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-            return cachedResponse || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
